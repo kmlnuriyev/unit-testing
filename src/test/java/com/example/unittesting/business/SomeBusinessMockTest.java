7d07d8db8a31eb809a85209e4083740624e12f54
kamalnuriyev <kemal.nuriyev@gmail.com>
2020-03-21 06:57:15 +0400

    Refactored test class - used mockito annotations

--- src/test/java/com/example/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/example/unittesting/business/SomeBusinessMockTest.java
@@ -1,22 +1,32 @@
 package com.example.unittesting.business;
 
 import com.example.unittesting.data.SomeDataService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
 
+    @InjectMocks
+    SomeBusinessImpl business;
+
+    @Mock
+    SomeDataService dataServiceMock;
+
+    /* We refactored with Mockito annotations..
     SomeBusinessImpl business = new SomeBusinessImpl();
     SomeDataService dataServiceMock = mock(SomeDataService.class);
 
     @BeforeEach
     public void before() {
         business.setSomeDataService(dataServiceMock);
-    }
+    }*/
 
     @Test
     void calculateSumUsingDataService_basic() {
